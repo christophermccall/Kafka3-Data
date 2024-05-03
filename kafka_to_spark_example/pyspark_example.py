131fe7f4a280f5d3689259f9b4d2501c95666234
christophermccall <camccall24@gmail.com>
2024-05-02 22:07:04 -0400

    making schema to set up for aggregations

--- kafka_to_spark_example/pyspark_example.py
+++ kafka_to_spark_example/pyspark_example.py
@@ -1,16 +1,14 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, expr
-from pyspark.sql.streaming import StreamingQuery
-import os
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, DoubleType
+from pyspark.sql.functions import col, from_json
 
 
-# os.system('spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 pyspark_example.py')
+# 'spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 pyspark_example.py'
 
 spark = SparkSession.builder \
     .appName("kafka_to_spark_example") \
     .getOrCreate()
 
-# use spark to create a new DataFrame from the kafka message
 
 df = spark \
     .readStream \
@@ -19,6 +17,37 @@ df = spark \
     .option("subscribe", "listen-activity") \
     .load()
 
+
+# create your schema based on the keys in the json
+schema = StructType([
+    StructField("artist", StringType(), True),
+    StructField("song", StringType(), True),
+    StructField("duration", DoubleType(), True),
+    StructField("ts", LongType(), True),
+    StructField("sessionId", IntegerType(), True),
+    StructField("auth", StringType(), True),
+    StructField("level", StringType(), True),
+    StructField("itemInSession", IntegerType(), True),
+    StructField("city", StringType(), True),
+    StructField("zip", StringType(), True),
+    StructField("state", StringType(), True),
+    StructField("userAgent", StringType(), True),
+    StructField("lon", DoubleType(), True),
+    StructField("lat", DoubleType(), True),
+    StructField("userId", IntegerType(), True),
+    StructField("lastname", StringType(), True),
+    StructField("firstname", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("registration", LongType(), True)
+])
+
+
+# use spark to create a new DataFrame from the kafka message
+
+
+
+
+
 query = df \
     .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
     .writeStream \
@@ -26,3 +55,4 @@ query = df \
     .outputMode("append") \
     .start()
 query.awaitTermination()
+
